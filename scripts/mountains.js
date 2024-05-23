f0103bd9ee961340056b605964dd1cd258baba59
CodeXand3rr <jballesteross235@gmail.com>
2024-05-23 17:44:17 -0400

    CapstoneV2

---
+++ scripts/mountains.js
@@ -0,0 +1,48 @@
+"use strict";
+
+window.onload = function () {
+  const mountainTB = document.querySelector("#mountainTB");
+  const mountainList = document.querySelector("#mountainList");
+
+  function loadMountains() {
+    mountainTB.innerText = "";
+    for (const mountain of mountainsArray) {
+      buildTableRow(mountain);
+    }
+  }
+  function buildMountainOption() {
+    for (const mountain of mountainsArray) {
+      const mountainOption = document.createElement("option");
+      mountainOption.value = mountain.name;
+      mountainOption.innerText = mountain.name;
+      mountainList.appendChild(mountainOption);
+    }
+  }
+  function buildTableRow(mountain) {
+    if (mountain.name == mountainList.value) {
+      let row = mountainTB.insertRow();
+
+      let cell1 = row.insertCell();
+      cell1.innerText = mountain.name;
+
+      let cell2 = row.insertCell();
+      cell2.innerText = mountain.elevation;
+
+      let cell3 = row.insertCell();
+      cell3.innerText = mountain.effort;
+
+      let cell4 = row.insertCell();
+      cell4.innerText = mountain.desc;
+
+      let img = document.createElement("img");
+      img.src = mountain.img;
+      img.alt = mountain.name;
+      img.width = 400;
+      cell4.appendChild(img);
+    }
+  }
+
+  mountainList.onchange = loadMountains;
+  loadMountains();
+  buildMountainOption();
+};
